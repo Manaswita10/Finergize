f1e4c446320010547deb1743a05328c20d679829
manaswita <manaswita.ch10@gmail.com>
2025-04-18 00:11:46 +0530

    comm savings

--- components/SketchfabModel.tsx
+++ components/SketchfabModel.tsx
@@ -174,7 +174,7 @@ const SketchfabModel: React.FC<SketchfabModelProps> = ({ modelPath }) => {
       ref={containerRef} 
       className="w-full h-full min-h-[300px] relative"
     >
-      {/* Loading indicator - only show when loading */}
+      {/* Loading indicator */}
       {isLoading && (
         <div className="absolute inset-0 flex items-center justify-center z-10">
           <div className="w-12 h-12 rounded-full border-4 border-blue-500/20 border-t-blue-500 animate-spin"></div>
